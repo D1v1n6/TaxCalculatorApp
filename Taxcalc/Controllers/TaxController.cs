74303c4f3f4e5b716dc12ad2ebcc939873b5bb37
Divine <dchukwudire@gmail.com>
2025-11-21 19:37:16 +0100

    Initial commit

---
+++ Taxcalc/Controllers/TaxController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taxcalc.Models;
+
+namespace Taxcalc.Controllers
+{
+    public class TaxController : Controller
+    {
+        [HttpGet]
+        public ActionResult Calculate()
+        {
+            return View(new TaxModel());
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(TaxModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CalculateTax();
+                ViewBag.Result = $"Estimated tax owed in {model.Country}: ₦{model.TaxOwed:N2}";
+            }
+
+            return View(model);
+        }
+    }
+}
+
---
+++ Taxcalc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taxcalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
